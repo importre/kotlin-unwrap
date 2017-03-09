316fb5a921ccbd282cfc919ac0c3907004f40356
Jaewe Heo <jaeweheo@gmail.com>
2017-03-09 21:18:03 +0900

    Cleanup code, drop functions that have 6~10 params respectively
    issue: #3

--- src/main/kotlin/com/importre/unwrap/Unwrap.kt
+++ src/main/kotlin/com/importre/unwrap/Unwrap.kt
@@ -1,10 +1,47 @@
 package com.importre.unwrap
 
-class Unwrap {
-
-    var valid: Boolean = false
-
-    infix fun <R> nah(f: Unwrap.() -> R) {
+class Unwrap(private var valid: Boolean) {
+    infix fun <R> nah(f: () -> R) {
         if (!valid) f()
     }
+}
+
+inline fun <T1, R> unwrap(
+        t1: T1?,
+        block: (T1) -> R): Unwrap {
+    val valid = null !in arrayOf<Any?>(t1)
+    if (valid) block(t1!!)
+    return Unwrap(valid = valid)
+}
+
+inline fun <T1, T2, R> unwrap(
+        t1: T1?, t2: T2?,
+        block: (T1, T2) -> R): Unwrap {
+    val valid = null !in arrayOf(t1, t2)
+    if (valid) block(t1!!, t2!!)
+    return Unwrap(valid = valid)
+}
+
+inline fun <T1, T2, T3, R> unwrap(
+        t1: T1?, t2: T2?, t3: T3?,
+        block: (T1, T2, T3) -> R): Unwrap {
+    val valid = null !in arrayOf(t1, t2, t3)
+    if (valid) block(t1!!, t2!!, t3!!)
+    return Unwrap(valid = valid)
+}
+
+inline fun <T1, T2, T3, T4, R> unwrap(
+        t1: T1?, t2: T2?, t3: T3?, t4: T4?,
+        block: (T1, T2, T3, T4) -> R): Unwrap {
+    val valid = null !in arrayOf(t1, t2, t3, t4)
+    if (valid) block(t1!!, t2!!, t3!!, t4!!)
+    return Unwrap(valid = valid)
+}
+
+inline fun <T1, T2, T3, T4, T5, R> unwrap(
+        t1: T1?, t2: T2?, t3: T3?, t4: T4?, t5: T5?,
+        block: (T1, T2, T3, T4, T5) -> R): Unwrap {
+    val valid = null !in arrayOf(t1, t2, t3, t4, t5)
+    if (valid) block(t1!!, t2!!, t3!!, t4!!, t5!!)
+    return Unwrap(valid = valid)
 }
--- src/test/kotlin/com/importre/unwrap/test_unwrap.kt
+++ src/test/kotlin/com/importre/unwrap/test_unwrap.kt
@@ -1,13 +1,45 @@
 package com.importre.unwrap
 
 import org.junit.Test
-import unwrap
 import kotlin.test.assertEquals
 
 class UnwrapTest {
 
     class Dummy1(val value: Int)
     class Dummy2(val value: String)
+
+    @Test
+    fun testOne() {
+        run {
+            val _a = Dummy1(1)
+            var validCount = 0
+            var invalidCount = 0
+
+            unwrap(_a) { a ->
+                validCount++
+            } nah {
+                invalidCount++
+            }
+
+            assertEquals(1, validCount)
+            assertEquals(0, invalidCount)
+        }
+
+        run {
+            val _a = null
+            var validCount = 0
+            var invalidCount = 0
+
+            unwrap(_a) { a ->
+                validCount++
+            } nah {
+                invalidCount++
+            }
+
+            assertEquals(0, validCount)
+            assertEquals(1, invalidCount)
+        }
+    }
 
     @Test
     fun testWithoutNah() {
@@ -77,5 +109,76 @@ class UnwrapTest {
 
         assertEquals(1, validCount)
         assertEquals(0, invalidCount)
+    }
+
+    @Test
+    fun test4Params() {
+        run {
+            var validCount = 0
+            var invalidCount = 0
+
+            unwrap(1, 2, 3, 4) { a, b, c, d ->
+                assertEquals(a, 1)
+                assertEquals(b, 2)
+                assertEquals(c, 3)
+                assertEquals(d, 4)
+                validCount++
+            } nah {
+                invalidCount++
+            }
+
+            assertEquals(1, validCount)
+            assertEquals(0, invalidCount)
+        }
+
+        run {
+            var validCount = 0
+            var invalidCount = 0
+
+            unwrap(1, 2, 3, null) { a, b, c, d ->
+                validCount++
+            } nah {
+                invalidCount++
+            }
+
+            assertEquals(0, validCount)
+            assertEquals(1, invalidCount)
+        }
+    }
+
+    @Test
+    fun test5Params() {
+        run {
+            var validCount = 0
+            var invalidCount = 0
+
+            unwrap("h", "e", 'l', 'l', "o") { a, b, c, d, e ->
+                assertEquals(a, "h")
+                assertEquals(b, "e")
+                assertEquals(c, 'l')
+                assertEquals(d, 'l')
+                assertEquals(e, "o")
+                validCount++
+            } nah {
+                invalidCount++
+            }
+
+            assertEquals(1, validCount)
+            assertEquals(0, invalidCount)
+        }
+
+        run {
+            var validCount = 0
+            var invalidCount = 0
+
+            unwrap(1, 2, 3, null, 5) { a, b, c, d, e ->
+                validCount++
+            } nah {
+                invalidCount++
+            }
+
+            assertEquals(0, validCount)
+            assertEquals(1, invalidCount)
+        }
     }
 }
